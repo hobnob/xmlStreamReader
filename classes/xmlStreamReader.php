8877ae6ab5f0c5b9961e2c02b755422440a46726
hobnob <bigchimp2005+github@gmail.com>
2013-03-20 13:46:14 +0000

    Moves the _parseString method
    Moves the _parseString method so that it is further down, with the protected methods

--- classes/xmlStreamReader.php
+++ classes/xmlStreamReader.php
@@ -29,24 +29,13 @@ class xmlStreamReader
         }
         else
         {
-            $this->_parseString( $parser, $chunk, TRUE );
+            $this->_parseString( $parser, $data, TRUE );
         }
         
-        xml_parser_free($xml_parser);
+        xml_parser_free($parser);
         return $this;
     }
 
-    protected function _parseString( $parser, $data, $isFinal )
-    {
-        if (!xml_parse($parser, $data, $isFinal))
-        {
-            throw new Exception(
-                xml_error_string( xml_get_error_code( $xml_parser ) )
-                .' At line: '.
-                xml_get_current_line_number( $xml_parser )
-            );
-        }
-    }
     public function registerCallback( $namespace, $callback )
     {
         if ( !is_string( $namespace ) )
@@ -70,6 +59,18 @@ class xmlStreamReader
         );
 
         return $this;
+    }
+
+    protected function _parseString( $parser, $data, $isFinal )
+    {
+        if (!xml_parse($parser, $data, $isFinal))
+        {
+            throw new Exception(
+                xml_error_string( xml_get_error_code( $parser ) )
+                .' At line: '.
+                xml_get_current_line_number( $parser )
+            );
+        }
     }
 
     protected function _start( $parser, $tag, $attributes )
